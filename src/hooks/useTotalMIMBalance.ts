fe96be25a76e26d3614d2bb62b942ad160af9ba7
Olof <enstromolof@gmail.com>
2022-01-04 10:58:39 +0100

    Fix total mim stuff

--- src/hooks/useTotalMIMBalance.ts
+++ src/hooks/useTotalMIMBalance.ts
@@ -3,45 +3,50 @@ import { useMetrics } from "../contexts";
 
 // Combines all MIM balances across subgraphs
 export const useTotalMIMBalance = () => {
-  const { avalancheData, ethereumWalletBalances } = useMetrics();
+  const { avalancheData, ethereumWalletBalances: ethereumData } = useMetrics();
   const mergedMIMBalance: TotalMIMBalanceMetric[] | null = useMemo(() => {
     // okay so we have two arrays of different lengths. one assumption we can make is that they end at the same date
     // however if lengths are different their internal data starts on different dates.
     //so, steps:
-    // 1. find shortest length.
-    // 2: slice all arrs to this min length (from the end, stepping toward i=0)
-    // 3. merge values knowing that all arrs are of same length
+    // 1. reverse arrays so that the first element is the latest data point
+    // 2. get the longest array and use as reference, loop over it
+    // 3. for each index, add all mim values from all arrays that have values at the current index.
 
-    const minLength = Math.min(
-      avalancheData.length,
-      ethereumWalletBalances.length
-    );
-    const shortAvalancheData = avalancheData.slice(
-      avalancheData.length - minLength
-    );
-    const shortEthereumData = ethereumWalletBalances.slice(
-      ethereumWalletBalances.length - minLength
-    );
+    const reversedAvalancheData = [...avalancheData].reverse();
+    const reversedEthereumData = [...ethereumData].reverse();
 
-    console.log(avalancheData, shortAvalancheData);
+    const arrays = [reversedAvalancheData, reversedEthereumData];
+    const longestArrIndex = arrays
+      .map((a) => a.length)
+      .indexOf(Math.max(...arrays.map((a) => a.length)));
+
+    const longestArr = arrays[longestArrIndex];
 
     const arr = [];
     // loop over each index
-    for (let i = 0; i < shortAvalancheData.length; i++) {
+    for (let i = longestArr.length - 1; i >= 0; i--) {
       const metadata = {
-        id: shortAvalancheData[i].id,
-        timestamp: shortAvalancheData[i].timestamp,
+        id: longestArr[i].id,
+        timestamp: longestArr[i].timestamp,
       };
-      const avaxMIM = shortAvalancheData[i].treasuryMIMMarketValue ?? 0;
-      const ethMIM = shortEthereumData[i].MIMCount ?? 0;
+
+      let indexMIM = 0;
+      if (reversedAvalancheData.length > i) {
+        indexMIM += reversedAvalancheData[i].treasuryMIMMarketValue ?? 0;
+      }
+
+      if (reversedEthereumData.length > i) {
+        indexMIM += reversedEthereumData[i].MIMCount ?? 0;
+      }
+
       arr.push({
         ...metadata,
-        MIMCount: avaxMIM + ethMIM,
+        MIMCount: indexMIM,
       });
     }
 
     return arr;
-  }, [avalancheData, ethereumWalletBalances]);
+  }, [avalancheData, ethereumData]);
 
   return mergedMIMBalance;
 };
